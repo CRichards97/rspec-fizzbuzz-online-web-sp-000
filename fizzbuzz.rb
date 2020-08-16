275f7b4a38e8a36421a2cb35109035f14a677cce
Clint Richards <richardsclint17@gmail.com>
2020-08-16 22:31:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def FizzBuzz(int)
+def fizzbuzz(int)
     if ((int % 5 == 0) && (int % 3 == 0))
        "FizzBuzz"
     elsif (int % 3 == 0)
