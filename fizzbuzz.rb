b2b4182a7197b35b44fc7775063aaac88d3bb52a
Clint Richards <richardsclint17@gmail.com>
2020-08-16 22:23:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  elsif condition
+     int % 5 == 0 # if the number int is divisible by 3
+    "buzz" # Go fizz
+  end
+end
